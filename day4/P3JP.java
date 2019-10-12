36f97d656b566f6176c4933a9227b5e8e126e22a
Naren Chandra <44522774+narenchandra859@users.noreply.github.com>
2019-10-12 22:19:44 +0530

    Add files via upload

---
+++ day4/P3JP.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+class Data 
+{
+    public boolean flag = true;
+    public void display_java() 
+    {
+        while(true)
+        {   
+            synchronized(this)
+            {
+                while (!flag) 
+                {
+                    try 
+                    { 
+                        wait();
+                    }
+                    catch (Exception e) {}
+                }
+                flag = false;
+                System.out.print("JAVA");
+                notify();
+            }
+        }
+    }
+    public void display_programming() 
+    {
+        while(true){   
+            synchronized(this)
+            {
+                while(flag) 
+                {
+                    try 
+                    { 
+                        wait();
+                    }
+                    catch (Exception e) {} 
+                }
+                flag = true;
+                System.out.println(" PROGRAMMING");
+                notify();
+            }
+        }
+    }
+}
+class jThread extends Thread
+{   
+    Data obj1;
+    jThread(Data obj)
+    {
+        obj1=obj;
+    }
+    public void run()
+    {
+        obj1.display_java();
+    }
+}
+class pThread extends Thread
+{   
+    Data obj1;
+    pThread(Data obj)
+    {
+        obj1=obj;
+    }
+    public void run()
+    {
+        obj1.display_programming();
+    }
+} 
+
+public class P3JP {
+    public static void main(String args[])
+    {
+	Data obj=new Data();
+        jThread t1 =new jThread(obj);
+        pThread t2 =new pThread(obj);
+        t1.start();
+        t2.start();
+    }
+}
